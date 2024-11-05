171b79ab4b653bae363ff6420cf51c6b1e78632f
june349 <176018493+june349@users.noreply.github.com>
2024-11-05 17:22:42 -0500

    add settings

---
+++ sealbot/settings.py
@@ -0,0 +1,9 @@
+from pydantic import Field
+from pydantic_extra_types.color import Color
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    token: str = Field(default=None)
+    embed_color: Color = Field(default="#7CB9E8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", frozen=True)
