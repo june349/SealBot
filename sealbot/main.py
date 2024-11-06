9dd604226f70a7ca311bebdedc6217f4ca2666f2
june349 <176018493+june349@users.noreply.github.com>
2024-11-06 16:21:44 -0500

    add main file

---
+++ sealbot/main.py
@@ -0,0 +1,31 @@
+import logging
+import sys
+
+import discord
+from pydantic import ValidationError
+
+from sealbot.bot import SealBot
+from sealbot.settings import Settings
+
+discord.utils.setup_logging()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+def main():
+    try:
+        bot = SealBot(settings=Settings(), intents=intents)
+        bot.run(bot.settings.token)
+    except ValidationError as e:
+        for err in e.errors():
+            location = '.'.join(map(str, err['loc']))
+            message = err['msg']
+            error_type = err['type']
+            logging.error(f"Settings validation error for '{location}': {message} ({error_type})")
+        sys.exit(1)
+    except discord.LoginFailure as e:
+        logging.error(f"Login failed: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
