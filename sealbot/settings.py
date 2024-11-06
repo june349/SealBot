71de13ca68d17de29a0f98796a426dffc88d3593
june349 <176018493+june349@users.noreply.github.com>
2024-11-06 13:58:21 -0500

    add prefix to settings

--- sealbot/settings.py
+++ sealbot/settings.py
@@ -5,5 +5,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     token: str = Field(default=...)
+    prefix: str = Field(default="+", min_length=1, max_length=1)
     embed_color: Color = Field(default="#7CB9E8")
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", frozen=True)
