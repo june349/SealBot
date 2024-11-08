1ae066ea65a77cd692c7a45da0d456977cb9bdfc
june349 <176018493+june349@users.noreply.github.com>
2024-11-08 11:46:21 -0500

    add assets_dir to settings

--- sealbot/settings.py
+++ sealbot/settings.py
@@ -1,4 +1,4 @@
-from pydantic import Field
+from pydantic import Field, DirectoryPath
 from pydantic_extra_types.color import Color
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -6,5 +6,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     token: str = Field(default=...)
     prefix: str = Field(default="+", min_length=1, max_length=1)
+    assets_dir: DirectoryPath = Field(default=...)
     embed_color: Color = Field(default="#7CB9E8")
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", frozen=True)
